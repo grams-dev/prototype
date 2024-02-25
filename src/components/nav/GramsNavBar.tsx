9153529a519293c99ec3d4a55f95bb8809f75dfd
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2024-02-24 19:59:45 -0800

    Drawer WIP

--- src/components/nav/GramsNavBar.tsx
+++ src/components/nav/GramsNavBar.tsx
@@ -2,24 +2,15 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import { GramsConnect } from '..';
+import { GramsDrawer } from './GramsDrawer';
 
 export const GramsNavBar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color='secondary'>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <GramsDrawer />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Grams
           </Typography>
