f21342b31680e6cf1c679cfa0f268d3910ebdbe4
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2024-02-25 19:54:17 -0800

    Update GramsDrawer.tsx

--- src/components/nav/GramsDrawer.tsx
+++ src/components/nav/GramsDrawer.tsx
@@ -7,12 +7,67 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import { IconButton } from '@mui/material';
 
+import HomeIcon from '@mui/icons-material/Home';
+import WalletIcon from '@mui/icons-material/Wallet';
+import ChatIcon from '@mui/icons-material/Chat';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import GroupIcon from '@mui/icons-material/Group';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import InfoIcon from '@mui/icons-material/Info';
+
+const menuLists = [
+    [
+        {
+            title: 'Home',
+            icon: <HomeIcon />,
+            link: '/'
+        },
+        {
+            title: 'Wallet',
+            icon: <WalletIcon />,
+            link: '/wallet'
+        },
+        {
+            title: 'Chat',
+            icon: <ChatIcon />,
+            link: '/chat'
+        },
+        {
+            title: 'Marketplace',
+            icon: <ShoppingCartIcon />,
+            link: '/marketplace'
+        },
+        {
+            title: 'Groups',
+            icon: <GroupIcon />,
+            link: '/groups'
+        }
+    ],
+    [
+        {
+            title: 'Settings',
+            icon: <SettingsIcon />,
+            link: '/settings'
+        },
+        {
+            title: 'Profile',
+            icon: <AccountCircleIcon />,
+            link: '/profile'
+        },
+        {
+            title: 'About',
+            icon: <InfoIcon />,
+            link: '/about'
+        }
+    ]
+]
+
 export const GramsDrawer = () => {
+
     const [open, setOpen] = React.useState(false);
 
     const toggleDrawer = (newOpen: boolean) => () => {
@@ -21,31 +76,23 @@ export const GramsDrawer = () => {
 
     const DrawerList = (
         <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
+            {menuLists.map((menuList, index) => (
+                <>
+                <List key={index}>
+                    {menuList.map((menu) => (
+                        <ListItem key={menu.title} disablePadding>
+                            <ListItemButton href={menu.link}>
+                                <ListItemIcon>
+                                    {menu.icon}
+                                </ListItemIcon>
+                                <ListItemText primary={menu.title} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                </>
+            ))}
         </Box>
     );
 
